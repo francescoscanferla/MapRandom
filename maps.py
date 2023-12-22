951ddadd8797a723a222ef71cd14fef40a5688da
Francesco Scanferla <info@francescoscanferla.it>
2023-12-22 13:21:13 +0100

    changed logic of creating sv_map_rotation command

--- maps.py
+++ maps.py
@@ -4,11 +4,13 @@ import random
 maps_cfg_path = 'Mods/ModWarfare'
 maps_cfg_filename = 'maps.cfg'
 maps_folder = "usermaps"
-command = "gametype koth map "
-default_maps = ["mp_convoy", "mp_backlot", "mp_bloc", "mp_bog", "mp_countdown", "mp_crash", "mp_crossfire",
-                "mp_district", "mp_downpour", "mp_overgrown", "mp_pipeline", "mp_shipment", "mp_showdown",
-                "mp_strike",
-                "mp_vacant", "mp_wetwork"]
+command = "gametype"
+gametype = "koth map"
+default_maps = ["mp_backlot", "mp_bloc", "mp_bog", "mp_carentan",
+                "mp_cargoship", "mp_citystreets", "mp_convoy", "mp_countdown",
+                "mp_crash", "mp_crash_snow", "mp_creek", "mp_crossfire",
+                "mp_farm", "mp_killhouse", "mp_overgrown", "mp_pipeline",
+                "mp_shipment", "mp_showdown", "mp_strike", "mp_vacant"]
 
 
 def change_map_rotation(sv_map_rotation):
@@ -26,6 +28,12 @@ def change_map_rotation(sv_map_rotation):
         file.writelines(lines)
 
 
+def generate_command(server_maps):
+    sliced_server_maps = server_maps[:20]
+    command_maps = [f"{gametype} {map}" for map in sliced_server_maps]
+    return f"{command} {' '.join(command_maps)}"
+
+
 def generate_random_map():
     result = {'default': len(default_maps)}
     custom_maps = os.listdir(maps_folder)
@@ -33,6 +41,6 @@ def generate_random_map():
     server_maps = default_maps + custom_maps
     result['total'] = len(server_maps)
     random.shuffle(server_maps)
-    sv_map_rotation = command + " ".join(server_maps)
+    sv_map_rotation = generate_command(server_maps)
     change_map_rotation(sv_map_rotation)
     return result
